85959d0884479396ddfc7a758aaba1bd57f3cef3
eason <9338akira@gmail.com>
2023-12-15 10:13:48 +0800

    更改express的讀檔路徑，將兩個前端頁面串連
    將searchResult改為靜態網頁，並調整讀檔路徑，完成兩個頁面串連

--- FrontEnd/public/javascripts/test.js
+++ FrontEnd/public/javascripts/test.js
@@ -2,7 +2,7 @@ function searchResult() {
     // 重定向到搜索结果介面
     window.location.href = '/searchResult';
 }
-document.getElementById('search-button').addEventListener('click', function(){
+document.querySelector('.search-button').addEventListener('click', function(){
     let input = document.getElementById("user-input").value;
     
     fetch('http://localhost:8080/add', {
