8d8bc37e2bf4bf2785c1c04157ec99ccc95059fe
eason <9338akira@gmail.com>
2023-12-16 22:50:04 +0800

    修改bug
    檔案名稱有錯，js的function用法有錯，已修正

--- FrontEnd/public/javascripts/searchResult.js
+++ FrontEnd/public/javascripts/searchResult.js
@@ -1,6 +1,6 @@
 function clearSearch(){
     //找到搜尋欄的input
-    var userInput = document.getElementById('user-input');
+    var userInput = document.querySelector('.search-input').value;
 
     //清空搜尋欄的input
     userInput.value = '';
