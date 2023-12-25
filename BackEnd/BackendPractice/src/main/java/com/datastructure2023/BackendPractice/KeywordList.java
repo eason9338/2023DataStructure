833c7e6ee721d8c5ad0cb9823a4b08744785dad4
eason <9338akira@gmail.com>
2023-12-25 13:24:04 +0800

    將jsoup引入maven

--- BackEnd/BackendPractice/src/main/java/com/datastructure2023/BackendPractice/KeywordList.java
+++ BackEnd/BackendPractice/src/main/java/com/datastructure2023/BackendPractice/KeywordList.java
@@ -13,37 +13,37 @@ public class KeywordList {
 		lst.add(keyword);
     }
 	
-	//Quick sort
-	public void sort(){
-		if(lst.size() == 0)
-		{
-			System.out.println("InvalidOperation");
-		}
-		else 
-		{
-			quickSort(0, lst.size()-1);
-		}
-	}
+	// //Quick sort
+	// public void sort(){
+	// 	if(lst.size() == 0)
+	// 	{
+	// 		System.out.println("InvalidOperation");
+	// 	}
+	// 	else 
+	// 	{
+	// 		quickSort(0, lst.size()-1);
+	// 	}
+	// }
 	
-	private void quickSort(int leftbound, int rightbound){
-		if (leftbound < rightbound) 
-		{
-			int i = leftbound - 1;
+	// private void quickSort(int leftbound, int rightbound){
+	// 	if (leftbound < rightbound) 
+	// 	{
+	// 		int i = leftbound - 1;
 			
-			for (int j = leftbound; j <= rightbound - 1; j++) 
-			{
-				if (lst.get(j).count < lst.get(rightbound).count) 
-				{
-					i++;
-					swap(i, j);
-				}
-			}
-			swap((i+1), rightbound);
+	// 		for (int j = leftbound; j <= rightbound - 1; j++) 
+	// 		{
+	// 			if (lst.get(j).count < lst.get(rightbound).count) 
+	// 			{
+	// 				i++;
+	// 				swap(i, j);
+	// 			}
+	// 		}
+	// 		swap((i+1), rightbound);
 
-			quickSort(leftbound, i);
-			quickSort(i + 2, rightbound);
-		}
-	}
+	// 		quickSort(leftbound, i);
+	// 		quickSort(i + 2, rightbound);
+	// 	}
+	// }
 
 	private void swap(int aIndex, int bIndex){
 		Keyword temp = lst.get(aIndex);
