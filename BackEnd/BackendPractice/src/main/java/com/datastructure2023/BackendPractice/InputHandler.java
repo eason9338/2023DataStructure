02c286a30a2e85c134ab3499b3da097d3c71d222
Lockrain0309 <118896468+Lockrain0309@users.noreply.github.com>
2023-12-25 23:57:23 +0800

    遊歷母子網並建成tree

--- BackEnd/BackendPractice/src/main/java/com/datastructure2023/BackendPractice/InputHandler.java
+++ BackEnd/BackendPractice/src/main/java/com/datastructure2023/BackendPractice/InputHandler.java
@@ -1,6 +1,8 @@
 package com.datastructure2023.BackendPractice;
 
 import java.util.Map;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,48 @@ public class InputHandler {
         private String input;
        
         public String getInput() { return input; }
+      
+        /*
+        rootUrl是搜到的URL
+        String rootUrl = "https://example.com";
+
+        // 創建 WebCrawler 並調用 crawlWebTree 方法
+        WebCrawler webCrawler = new WebCrawler();
+        WebTree webTree = webCrawler.crawlWebTree(rootUrl);
+
+        // 設定keywordlist
+        ArrayList<Keyword> keywordList = new ArrayList<>();
+
+        Add keywords and their weights to the list
+        keywordList.add(new Keyword(input, 0.9f));
+        keywordList.add(new Keyword("實習", 0.9));
+        keywordList.add(new Keyword("實習生", 0.9));
+        keywordList.add(new Keyword("大一", 0.2));
+        keywordList.add(new Keyword("大二", 0.4));
+        keywordList.add(new Keyword("大三", 0.6));
+        keywordList.add(new Keyword("大四", 0.8));
+        keywordList.add(new Keyword("碩一", 0.9));
+        keywordList.add(new Keyword("碩二", 1.0));
+        keywordList.add(new Keyword("應屆畢業生", 1.0));
+        keywordList.add(new Keyword("兼職", 0.5));
+        keywordList.add(new Keyword("轉正職機會", 0.7));
+        keywordList.add(new Keyword("不限年資", 0.6));
+        keywordList.add(new Keyword("不需負擔管理責任", 0.8));
+        keywordList.add(new Keyword("職缺", 0.9));
+        keywordList.add(new Keyword("薪水", 0.5));
+        keywordList.add(new Keyword("勞保", 0.5));
+        keywordList.add(new Keyword("短期實習", 0.7));
+        keywordList.add(new Keyword("面試", 0.6));
+        keywordList.add(new Keyword("長期實習", 0.7));
+        keywordList.add(new Keyword("培訓", 0.6));
+
+        // 創建 WebTree 對象並調用 setPostOrderScore 方法
+        for(Keyword keywords:keywordList){
+                webTree.setPostOrderScore(keywords);
+        }
+       
+        }*/
+    
 
         public void setInput(String data) { input = data; }
     }
--- BackEnd/BackendPractice/src/main/java/com/datastructure2023/BackendPractice/WebPage.java
+++ BackEnd/BackendPractice/src/main/java/com/datastructure2023/BackendPractice/WebPage.java
@@ -3,25 +3,26 @@ package com.datastructure2023.BackendPractice;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class WebPage
-{
-	public String url;
-	public String name;
-	public WordCounter counter;
-	public double score;
+public class WebPage {
+     public String url;
+     public WordCounter counter;
+     public double score;
 
-	public WebPage(String url, String name)
-	{
-		this.url = url;
-		this.name = name;
-		this.counter = new WordCounter(url);
-	}
+     //改成存入url及其子網頁
+     public WebPage(String url) {
+         this.url = url;
+         this.counter = new WordCounter(url);
+     }
 
-	public void setScore(ArrayList<Keyword> keywords) throws IOException {
-    score = 0;
+	 public String getUrl(){
+		return this.url;
+	 }
+     // setScore 方法用於設定分數
+     public void setScore(ArrayList<Keyword> keywords) throws IOException {
+         score = 0;
 
-		for (Keyword k : keywords) {
-			score += k.weight * counter.countKeyword(k.name);
-		}
-	}
+         for (Keyword k : keywords) {
+             score += k.weight * counter.countKeyword(k.name);
+         }
+     }
 }
