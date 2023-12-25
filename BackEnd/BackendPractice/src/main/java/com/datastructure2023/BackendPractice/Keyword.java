ece7817657b2773799c3284d9d1eab376efb2a19
Lockrain0309 <118896468+Lockrain0309@users.noreply.github.com>
2023-12-25 13:22:22 +0800

    更新webList及權重

--- BackEnd/BackendPractice/src/main/java/com/datastructure2023/BackendPractice/Keyword.java
+++ BackEnd/BackendPractice/src/main/java/com/datastructure2023/BackendPractice/Keyword.java
@@ -24,3 +24,36 @@ public class Keyword {
     	return weight;
     }
 }
+
+// // ArrayList<Keyword> keywordList = new ArrayList<>();
+
+//         // Add keywords and their weights to the list
+//         keywordList.add(new Keyword(input, 0.9f));
+//         keywordList.add(new Keyword("實習", 0.9));
+//         keywordList.add(new Keyword("實習生", 0.9));
+//         keywordList.add(new Keyword("大一", 0.2));
+//         keywordList.add(new Keyword("大二", 0.4));
+//         keywordList.add(new Keyword("大三", 0.6));
+//         keywordList.add(new Keyword("大四", 0.8));
+//         keywordList.add(new Keyword("碩一", 0.9));
+//         keywordList.add(new Keyword("碩二", 1.0));
+//         keywordList.add(new Keyword("應屆畢業生", 1.0));
+//         keywordList.add(new Keyword("兼職", 0.5));
+//         keywordList.add(new Keyword("轉正職機會", 0.7));
+//         keywordList.add(new Keyword("不限年資", 0.6));
+//         keywordList.add(new Keyword("不需負擔管理責任", 0.8));
+//         keywordList.add(new Keyword("職缺", 0.9));
+//         keywordList.add(new Keyword("薪水", 0.5));
+//         keywordList.add(new Keyword("勞保", 0.5));
+//         keywordList.add(new Keyword("短期實習", 0.7));
+//         keywordList.add(new Keyword("面試", 0.6));
+//         keywordList.add(new Keyword("長期實習", 0.7));
+//         keywordList.add(new Keyword("培訓", 0.6));
+
+// 核心關鍵字是 '實習/實習生'（1.0），'職缺'(0.9)
+// 由於隨學年增加實習的需求亦增加，故學年越高權重越高，同時對應屆畢業生有高權重（1.0），
+// 考慮到兼職機會，'Part time' 權重為 0.5。而'轉正職機會'有利於職業發展（0.7），
+// '不限年資'、'不需負擔管理責任，可表示是初階職缺、非管理職，故工作更適配畢業生（0.7）。
+// '短期實習'，'長期實習'，由於與實習的時間相關（0.7）
+// 面試'（0.6）和 '培訓'（0.6），這兩個詞彙多出現在初階職缺(0.6)
+// 其餘關鍵字 '薪水'（0.3）、'勞保'（0.3），其餘相關性相對邊緣的都設0.3
--- BackEnd/BackendPractice/src/main/java/com/datastructure2023/BackendPractice/WebNode.java
+++ BackEnd/BackendPractice/src/main/java/com/datastructure2023/BackendPractice/WebNode.java
@@ -3,57 +3,53 @@ package com.datastructure2023.BackendPractice;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class WebNode
-{
-	public WebNode parent;
-	public ArrayList<WebNode> children;
-	public WebPage webPage;
-	public double nodeScore;// Webpage total Score
-	
-	//constructor
-	public WebNode(WebPage webPage)
-	{
-		this.webPage = webPage;
-		this.children = new ArrayList<WebNode>();
-	}
+public class WebNode {
+    public WebNode parent;               
+    public ArrayList<WebNode> children;  
+    public WebPage webPage;              
+    public double nodeScore;            
 
-	//計算網頁分數
-	public void setNodeScore(ArrayList<Keyword> keywords) throws IOException
-	{
-		webPage.setScore(keywords);
-		nodeScore = webPage.score;
-		for(WebNode w:children) {
-			nodeScore += w.nodeScore;
-		}
-		
-	}
+    // constructor
+    public WebNode(WebPage webPage) {
+        this.webPage = webPage;
+        this.children = new ArrayList<WebNode>();
+    }
 
-	//加入子網頁
-	public void addChild(WebNode child)
-	{
-		this.children.add(child);
-		child.parent = this;
-	}
+    // 根據關鍵字列表計算節點分數
+    public void setNodeScore(ArrayList<Keyword> keywords) throws IOException {
+       
+		// 根據關鍵字設定相關網頁的分數
+        webPage.setScore(keywords);
+        nodeScore = webPage.score;
 
-	//確定是否最後的子網業
-	public boolean isTheLastChild()
-	{
-		if (this.parent == null)
-			return true;
-		ArrayList<WebNode> siblings = this.parent.children;
+        // 把子網頁的分數做統計，計算整個網頁的總分
+        for (WebNode w : children) {
+            nodeScore += w.nodeScore;
+        }
+    }
 
-		return this.equals(siblings.get(siblings.size() - 1));
-	}
+    // 將子節點加入當前節點
+    public void addChild(WebNode child) {
+        this.children.add(child);
+        child.parent = this;
+    }
 
-	public int getDepth()
-	{
-		int retVal = 1;
-		WebNode currNode = this;
-		while (currNode.parent != null)
-		{
-			retVal++;
-			currNode = currNode.parent;
-		}
-		return retVal;
-	}
+    // 檢查當前節點是否為其父節點的最後一個子節點
+    public boolean isTheLastChild() {
+        if (this.parent == null)
+            return true;
+        ArrayList<WebNode> siblings = this.parent.children;
+        return this.equals(siblings.get(siblings.size() - 1));
+    }
+
+    // 取得節點在樹中的深度
+    public int getDepth() {
+        int retVal = 1;
+        WebNode currNode = this;
+        while (currNode.parent != null) {
+            retVal++;
+            currNode = currNode.parent;
+        }
+        return retVal;
+    }
 }
