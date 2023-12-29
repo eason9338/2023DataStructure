d1e5553e22849efcb56c52b33f0fa69b41be7240
eason <9338akira@gmail.com>
2023-12-29 22:34:22 +0800

    將搜尋結果改為20個

--- BackendTest/GoogleQuery.java
+++ BackendTest/GoogleQuery.java
@@ -3,41 +3,65 @@ import java.net.URL;
 import java.net.HttpURLConnection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class GoogleQuery {
     
     private String apiKey;
     private String searchEngineID;
-    public String query = "實習";
 
     public GoogleQuery(String apiKey, String searchEngineID) {
         this.apiKey = apiKey;
         this.searchEngineID = searchEngineID;
     }
 
-    public String search(String query) throws IOException {
-        String urlString = "https://www.googleapis.com/customsearch/v1?q=" + query + "&cx=" + searchEngineID + "&key=" + apiKey + "&lr=lang_zh-TW" + "&cr=countryTW";
+    public ArrayList<String> search(String query) throws IOException {
+
+        ArrayList<String> responseArray = new ArrayList<>();
+
+        String page1Url = buildSearchString(query, 1);
+        responseArray.add(performSearch(page1Url));
+
+        String page2Url = buildSearchString(query, 11);
+        responseArray.add(performSearch(page2Url));
+
+        return responseArray;
+    }
+
+    public String buildSearchString(String query, int start) {
+        return "https://www.googleapis.com/customsearch/v1?q=" + query
+               + "&cx=" + searchEngineID
+               + "&key=" + apiKey
+               + "&lr=lang_zh-TW"
+               + "&cr=countryTW"
+               + "&num=10"
+               + "&start=" + start;    
+    }
+
+    public String performSearch(String urlString) throws IOException{
+
         URL url = new URL(urlString);
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
         connection.setRequestMethod("GET");
         connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
-        // System.out.println("網址連結：" + urlString);
+
         int responseCode = connection.getResponseCode();
         if(responseCode == 200) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
             StringBuilder response = new StringBuilder();
-
             while((line = reader.readLine()) != null) {
                 response.append(line);
             }
 
             reader.close();
-            //response 就是 Google API 回應的JSON文檔
             return response.toString();
-        } else {
-            return "Response returned with error code: " + responseCode;
-        }
 
+        } else {
+            return "Error Code: " + responseCode;
+        }
+        
     }
+
 }
--- BackendTest/Main.java
+++ BackendTest/Main.java
@@ -7,54 +7,61 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // String rootUrl = "https://www.104.com.tw/jobs/main/"; 
-        // ArrayList<Keyword> keywordList = new ArrayList<Keyword>();
+        String rootUrl = "https://www.104.com.tw/jobs/main/students/?tab=parttime&keyword=%E5%AF%A6%E7%BF%92&ro=2&pagesize=50&jobsource=students_PC&showDutyTime=1&hotJob=0&order=12&asc=0"; 
+        ArrayList<Keyword> keywordList = new ArrayList<Keyword>();
 
-        // try {
-        //     File file = new File("Keywords.txt");
-        //     Scanner sc = new Scanner(file);
-        //     while (sc.hasNext()) {
-        //         String name = sc.next();
-        //         float weight = Float.parseFloat(sc.next());
-        //         Keyword newKeyword = new Keyword(name, weight);
-        //         keywordList.add(newKeyword);
-        //     }
-        //     sc.close();
-        // } catch (FileNotFoundException e) {
-        //     System.out.println("找不到文件");
-        // }
+        try {
+            File file = new File("Keywords.txt");
+            Scanner sc = new Scanner(file);
+            while (sc.hasNext()) {
+                String name = sc.next();
+                float weight = Float.parseFloat(sc.next());
+                Keyword newKeyword = new Keyword(name, weight);
+                keywordList.add(newKeyword);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("找不到文件");
+        }
 
 
-        // WebCrawler webCrawler = new WebCrawler(rootUrl);
+        WebCrawler webCrawler = new WebCrawler(rootUrl);
 
-        // // 獲取建立的 WebTree 實例
-        // WebTree webTree = webCrawler.getWebTree();
+        // 獲取建立的 WebTree 實例
+        WebTree webTree = webCrawler.getWebTree();
 
-        // // 使用 WebTree 中的方法計算分數
-        // try {
-        //     webTree.setPostOrderScore(keywordList);
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
+        // 使用 WebTree 中的方法計算分數
+        try {
+            webTree.setPostOrderScore(keywordList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        // // 獲取計算後的總分
-        // double totalScore = Math.round(webTree.getTotalScore()* 10.0) / 10.0;
-        // System.out.println("Total Score: " + totalScore);
+        // 獲取計算後的總分
+        double totalScore = Math.round(webTree.getTotalScore()* 10.0) / 10.0;
+        System.out.println("Total Score: " + totalScore);
 
-        // // 打印網址列表
-        // webTree.setPostOrderScore(keywordList);
-        // //System.out.println(webTree.eulerPrintTree());
-        // webCrawler.printList();
+        // 打印網址列表
+        webTree.setPostOrderScore(keywordList);
+        //System.out.println(webTree.eulerPrintTree());
+        webCrawler.printList();
         
         //上面的部分output會是Total Score: 1.2
         //https://www.104.com.tw/jobs/main/
 
         //google api執行爬蟲部分，By 李淳澔，要上面功能救註解掉下面的
-        SearchResultHandler searchResultHandler = new SearchResultHandler("AIzaSyDaMRJAuiClBmf_TatB_3PMtsKqiRt18XM", "e618816a42f6949e9");
-        ArrayList<SearchResultItem> results = searchResultHandler.search("實習");
-        for(SearchResultItem item: results) {
-            System.out.print(item.getTitle());
-            System.out.println(item.getLink());
-        }
+        //SearchResultHandler searchResultHandler = new SearchResultHandler("AIzaSyDaMRJAuiClBmf_TatB_3PMtsKqiRt18XM", "e618816a42f6949e9");
+        // ArrayList<SearchResultItem> results = searchResultHandler.search("實習北部");
+        // for(SearchResultItem item: results) {
+        //     System.out.print(item.getTitle());
+        //     System.out.println(item.getLink());
+        // }
+
+        //SearchResultHandler searchResultHandler = new SearchResultHandler("AIzaSyDaMRJAuiClBmf_TatB_3PMtsKqiRt18XM", "e618816a42f6949e9");
+        // ArrayList<SearchResultItem> results = searchResultHandler.search("實習");
+        // for(SearchResultItem result: results) {
+        //     System.out.print(result.getTitle() + " ");
+        //     System.out.println(result.getLink());
+        // }
     }
 }
